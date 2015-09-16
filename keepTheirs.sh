92708eb581fb07fdde5d8d3fb91ab75ef963a500
victortelia <viktor.v.karlsson@teliasonera.com>
2015-09-16 15:03:22 +0200

    h

--- keepTheirs.sh
+++
@@ -1,5 +0,0 @@
-# I want to keep MY version when there is a conflict
-# Nothing to do: %A (the second parameter) already contains my version
-# Just indicate the merge has been successfully "resolved" with the exit status
-cp -f $2 $3
-exit 0
